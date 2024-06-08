2b33be1449e7ef8b55f2e29adde18e79802c6500
abir <abirstitou213@gmail.com>
2024-06-08 20:20:55 +0100

    changement de couleur

--- vue9/src/components/AccueilVue.vue
+++ vue9/src/components/AccueilVue.vue
@@ -155,7 +155,7 @@ body, html {
   left: 41%; 
   width: 41%; 
   height: 100%;
-  background: blue;
+  background: orange;
   border-bottom-right-radius: 200px; 
   transform: translateX(-100%); 
 }
